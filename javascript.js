3fdea39c3f5024fdf45b7b7d3ea0bc548f1a5625
chaswrig <42904530+chaswrig@users.noreply.github.com>
2025-01-29 23:53:21 -0600

    Changed pixel logic. Window now starts with a 16 by 16 drawing surface. Erase button erases what's been drawn and resets the draw field. Pixel button not working but in future state will change the total number of pixels.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,27 @@
 let size = 16;
 let totalPixels = size*size;
 
+
 const surface = document.querySelector("#surface");
+createPixels();
 
 const pixelButton = document.querySelector("#pixels");
 pixelButton.addEventListener("click", () => {
+    //TODO -- CHANGE PIXEL COUNT AND SIZE
+});
+
+const eraseButton = document.querySelector("#eraser");
+eraseButton.addEventListener("click", () => {
+    eraseSurface();
+    createPixels();
+});
+
+function eraseSurface(){
+    const oldPixels = document.querySelectorAll(".pixel");
+    oldPixels.forEach(oldPixel => oldPixel.remove());
+};
+
+function createPixels(){
     for(let i = 1; i <= totalPixels; i++) {
         const div = document.createElement("div");
         div.classList.add("pixel");
@@ -13,4 +30,4 @@ pixelButton.addEventListener("click", () => {
         });
         surface.appendChild(div);
     }
-});
+};
